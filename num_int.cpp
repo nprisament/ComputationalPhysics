fcc0a8e612631716488c2c48d3b6e962a55115d6
Noah Theodore Prisament <nprisament@gmail.com>
2019-01-30 01:48:11 -0500

    hw1

--- num_int.cpp
+++ num_int.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <cmath>
+#include <limits>
 
 using namespace std;
 
 class function {
 public:
-	unsigned int operator()(double x) const {
-		return exp(x);
+	double operator()(double x) const {
+		return 3*x*x;
 	}
 };
 
@@ -20,9 +20,33 @@ double trap_rule(const function & f, double a, double b) {
 	return sum;
 }
 
+double simp(const function & f, double a, double b) {
+	double sum = 0, h = (b - a) / 10001.0, w = 1;
+	b = h * 10001.0 + a;
+	for (double i = a; i <= b; i += h) {
+		if ((i == a || i == b)) w = 1;
+		else if (w == 1 || w == 2) w = 4;
+		else w = 2;
+		sum += w * h * f(i) / 3.0;
+	}
+	return sum;
+}
+
+double guass(const function & f, double a, double b) {
+	double c1 = 0.5555555556, c2 = 0.8888888889, c3 = 0.5555555556;
+	double x1 = -0.774596669, x2 = 0.0, x3 = 0.774596669;
+	double half = (b - a) / 2;
+	x2 += (a + half);
+	x1 *= half; x1 += x2;
+	x3 *= half; x3 += x2;
+	return (c1 * f(x1) + c2 * f(x2) + c3 * f(x3)) * half;
+}
+
 int main(){
 	function f;
-	cout << trap_rule(f, 0, 1) << endl;
-	
+	cout.precision(numeric_limits<double>::max_digits10);
+	cout << trap_rule(f, 0, 2) << endl;
+	cout << simp(f, 0, 2) << endl;
+	cout << guass(f, 0, 2) << endl;
 	return 1;
 }
