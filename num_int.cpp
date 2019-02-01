9f069120a580758fd26b2e4c0120e9d7e1d9cce8
Noah Theodore Prisament <nprisament@gmail.com>
2019-02-01 18:51:09 -0500

    more hw1

--- num_int.cpp
+++ num_int.cpp
@@ -7,17 +7,17 @@ using namespace std;
 class function {
 public:
 	double operator()(double x) const {
-		return 7.2 * x * x * x * x - 4.0 * x * x + 2;
+		return 3.0 * x * x - 1.5 * x;//7.2 * x * x * x * x - 4.0 * x * x + 2;
 	}
 };
 
 double trap_rule(const function & f, double a, double b, double h) {
-	double sum = 0;
-	h = 1 / h;
-	for (double i = a; i <= b; i += h) {
+	double sum = 0, inc = (b - a) / h;
+	b = h * inc + a;
+	for (double i = a; i <= b; i += inc) {
 		double w = 1;
-		if (i == 0 || i == 1) w = 0.5;
-		sum += w * h * f(i);
+		if (i == 0 || i == b) w = 0.5;
+		sum += w * inc * f(i);
 	}
 	return sum;
 }
@@ -35,7 +35,7 @@ double simp(const function & f, double a, double b, double h) {
 	return sum;
 }
 
-double guass(const function & f, double a, double b) {
+double gauss(const function & f, double a, double b) {
 	double c1 = 0.5555555556, c2 = 0.8888888889, c3 = 0.5555555556;
 	double x1 = -0.774596669, x2 = 0.0, x3 = 0.774596669;
 	double half = (b - a) / 2;
@@ -45,22 +45,29 @@ double guass(const function & f, double a, double b) {
 	return (c1 * f(x1) + c2 * f(x2) + c3 * f(x3)) * half;
 }
 
+double localGauss(const function & f, double a, double b, double h) {
+	double sum = 0, inc = (b - a) / h;
+	b = h * inc + a;
+	for (double i = a; i < b; i += inc) sum += gauss(f, i, i + inc);
+	return sum;
+}
+
 int main(){
 	function f;
 	cout.precision(numeric_limits<double>::max_digits10);
-	double exact = 23758.746666666666, ans = 0;
+	double exact = 306.25, ans = 0;//23758.74666666666, ans = 0;
 	cout << "Trapezoidal Rule" << endl;
 	for(unsigned h = 10; h < 100; h += 10) {
 		ans = trap_rule(f, 0, 7, h);
-		cout << h << " " << ans << " " << abs(ans - exact) / exact << endl;
+		cout << h + 1.0 << " " << ans << " " << abs(ans - exact) / exact << endl;
 	}
 	for(unsigned h = 100; h < 1000; h += 100){
 		ans = trap_rule(f, 0, 7, h);
-		cout << h << " " << ans << " " << abs(ans - exact) / exact << endl;
+		cout << h + 1.0 << " " << ans << " " << abs(ans - exact) / exact << endl;
 	}
 	for(unsigned h = 1000; h <= 10000000; h *= 10) {
 		ans = trap_rule(f, 0, 7, h);
-		cout << h << " " << ans << " " << abs(ans - exact) / exact << endl;
+		cout << h + 1.0 << " " << ans << " " << abs(ans - exact) / exact << endl;
 	}
 	cout << "Simpson's Rule" << endl;
 	for(unsigned h = 10; h < 100; h += 10) {
@@ -76,6 +83,11 @@ int main(){
 		cout << h + 1.0 << " " << ans << " " << abs(ans - exact) / exact << endl;
 	}
 	cout << "Gaussian Quadrature" << endl;
-	cout << guass(f, 0, 7) << endl;
+	for(unsigned h = 1; h <= 10000000; h *= 10) {
+		ans = localGauss(f, 0, 7, h);
+		cout << h << " " << ans << " " << abs(ans - exact) / exact << endl;
+	}
 	return 1;
 }
+
+//		cout << "(" << h << "," << abs(ans - exact) / exact << ")" << endl;
